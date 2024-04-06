b022b199edef0e0d6c6b2e98ebe71d371a54d45e
Suyash-Rajput <66985650+Suyash-Rajput@users.noreply.github.com>
2024-04-06 13:25:23 +0530

    changes

--- backend/myApp/urls.py
+++ backend/myApp/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 
 urlpatterns= [
-    path('home.html', views.home_page, name = 'home_page'),
+    path('', views.home_page, name = 'home_page'),
     path('Contract.html', views.contract_page, name='contract_page'),   
     path('Hobbies.html', views.hobbie_page, name='hobbie_page'), 
     path('send-email', views.send_email, name='send_email'),
